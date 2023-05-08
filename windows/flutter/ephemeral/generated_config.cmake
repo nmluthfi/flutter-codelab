9a47a54116473aebefa76a7af7dd3554f46e4a47
Nur Muhammad Luthfi <nurmuhammadluthfi@gmail.com>
2023-05-08 10:28:35 +0700

    git commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\download\\flutter_windows_3.7.10-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\flutter-project\\flutter_codelab" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\download\\flutter_windows_3.7.10-stable\\flutter"
+  "PROJECT_DIR=C:\\flutter-project\\flutter_codelab"
+  "FLUTTER_ROOT=D:\\download\\flutter_windows_3.7.10-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\flutter-project\\flutter_codelab\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\flutter-project\\flutter_codelab"
+  "FLUTTER_TARGET=C:\\flutter-project\\flutter_codelab\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\flutter-project\\flutter_codelab\\.dart_tool\\package_config.json"
+)
